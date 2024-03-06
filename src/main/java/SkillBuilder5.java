704ac3fbf0e20441aeecb760a21ddd7b70aba1f9
Evans <earmantrading23@students.desu.edu>
2024-03-06 10:52:29 -0500

    Completed.

--- src/main/java/SkillBuilder5.java
+++ src/main/java/SkillBuilder5.java
@@ -1,14 +1,17 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * This class provides various Skill Builder 5 - Array
  * exercises.
  *
- * @author (you)
- * @version (a version number or a date)
+ * @author Evans
+ * @version 3/6/24
  */
 public class SkillBuilder5
 {
     /**
-     * Calculates the prefix average of array data and returns the prefex average
+     * Calculates the prefix average of array data and returns the prefix average
      * in a new array.  The parameter data is never touched and left intact.
      *
      * @param data array of double values on which to calculate the prefix average.
@@ -17,9 +20,16 @@ public class SkillBuilder5
      */
     public static double[] prefixAverage(double[] data)
     {
-        //TODO: replace this line with your code.
-        return null;
+        double[] avg = new double [data.length];
+        double q = 0;
+        for(int index = 0; index < data.length; index ++){
+            q += data[index];
+            avg[index] = q / (index + 1);
+        }
+
+        return avg;
     }
+
 
     /**
      * Finds the location in array anArray where value is located.  If anArray
@@ -31,8 +41,14 @@ public class SkillBuilder5
      */
     public static int  indexOf(int searchValue, int[] anArray)
     {
-        //TODO: replace this line with your code.
-        return 0;
+        int answer = -1;
+        for (int index = 0; index < anArray.length; index ++) {
+            if (searchValue == anArray[index]) {
+                answer = index;
+                break;
+            }
+        }
+        return answer;
     }
 
     /**
@@ -45,8 +61,14 @@ public class SkillBuilder5
      */
     public static int  indexOf(String s, String[] anArray)
     {
-        //TODO: replace this line with your code.
-        return 0;
+        int answer = -1;
+        for (int index = 0; index < anArray.length; index ++) {
+            if (s.equals(anArray[index])) {
+                answer = index;
+                break;
+            }
+        }
+        return answer;
     }
 
     /**
@@ -56,18 +78,42 @@ public class SkillBuilder5
      * @param anArray array in which to search and remove s
      * @return An array with all occurrences of s removed.
      */
-    public static String[] remove(String s, String[] anArray)
-    {
-        // add your code here
-        return null;
+    public static String[] remove(String s, String[] anArray) {
+        int count = 0;
+        // the for-loop finds the length that our removed version will have
+        for(int index = 0; index < anArray.length; index++) {
+            if (!s.equals(anArray[index])) {
+                count++;
+            }
+        }
+        int newIndex = 0;
+        String[] removed = new String[count];
+        // this for-loop adds the contents of the array to the removed version
+        for(int index = 0; index < anArray.length; index ++){
+            if (!s.equals(anArray[index])){
+                removed[newIndex] = anArray[index];
+                // new index only updates if s isn't anArray[index]
+                newIndex ++;
+            }
+
+        }
+        return removed;
     }
 
     /**
      * Reverses an array of integers.
      * @param anArray the array whose contents should be reversed.
      */
-    public static void reverse(int[] anArray)
-    {
-        // add your code here
+    public static void reverse(int[] anArray) {
+        int len = anArray.length;
+        //the condition is len / 2 because we are swapping 2 at a time
+        for (int index = 0; index < len / 2; index++) {
+            // temporarily holds the number at anArray[index]
+            int temp = anArray[index];
+            //swap anArray[index] with its equivalent in the reversed version
+            anArray[index] = anArray[len - index - 1];
+            anArray[len - index - 1] = temp;
+        }
     }
+    
 }
